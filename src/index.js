00c55d1c44be8de3d18279cb828ae05d96183b01
Sara-707 <sarasg@outlook.de>
2025-01-31 20:45:58 -0600

    api corrected again

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ function refreshWeather(response) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiKey = "83b247a2da23te3354f6da56o0ffdb52";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
