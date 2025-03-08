ac26b14d6b6edebaa515bc05b246ee8f29fc4e2a
Sara-707 <sarasg@outlook.de>
2025-03-08 14:45:20 -0600

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-function refreshWeather(response) {
-  console.log(response.data);
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-
-  cityElement.innerHTML = response.data.city;
-  timeElement.innerHTML = formatDate(date);
-  descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  temperatureElement.innerHTML = Math.round(temperature);
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+function refreshData(response) {
+  let temperature = document.querySelector("#now-temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  let icon = document.querySelector("#now-icon");
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" alt="weather-icon"></img>`;
+  let date = new Date(response.data.time * 1000);
+  let time = document.querySelector("#time");
+  time.innerHTML = formatDate(date);
+  let condition = document.querySelector("#now-condition");
+  condition.innerHTML = response.data.condition.description;
+  let humidity = document.querySelector("#now-humidity");
+  humidity.innerHTML = response.data.temperature.humidity;
+  let windspeed = document.querySelector("#now-windspeed");
+  windspeed.innerHTML = response.data.wind.speed;
 }
+
 
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -26,12 +27,9 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
+ 
+    
   let day = days[date.getDay()];
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
   return `${day} ${hours}:${minutes}`;
 }
 
@@ -39,16 +37,57 @@ function searchCity(city) {
   let apiKey = "83b247a2da23te3354f6da56o0ffdb52";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
   axios.get(apiUrl).then(refreshWeather);
-}
+} 
 
-function handleSearchSubmit(event) {
+function submitCity(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-
+  let searchInput = document.querySelector("#search-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "7043o377bb0t07ffedfea40179990f35";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="data-forecast">
+        <div class="forecast-day">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="forecast-icon" />
+        <div class="forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+
+          <div class="forecast-temperature-min">${Math.round(
+            day.temperature.minimum
+          )}º</div>
+        </div>
+      </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let searchElement = document.querySelector("#search-element");
+searchElement.addEventListener("submit", submitCity);
 
 searchCity("Paris");
